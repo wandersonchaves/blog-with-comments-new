e448f5be253b9ec0ab7a6dd5c6fbe02108416a66
WandersonChaves <wandersonscpibr@gmail.com>
2020-05-08 11:21:06 -0300

    Add Modal SignIN/OUT

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def resource_name
+    :account
+  end
+
+  def resource
+    @resource ||= Account.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:account]
+  end
 end
