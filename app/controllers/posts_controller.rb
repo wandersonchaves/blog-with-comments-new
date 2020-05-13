a14ed3220b14acb984b31ee7d201330be6797e1d
WandersonChaves <wandersonscpibr@gmail.com>
2020-05-13 18:01:12 -0300

    Finished Stilished New Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :description, :content)
   end
 end
